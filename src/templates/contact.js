38dfd26b07f878cb1d59da8146c0f6a07c4a836e
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2019-07-30 17:33:36 -0400

    edit  netlify form

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -13,37 +13,36 @@ const contact = ({ pageContext }) => (
             fluid={pageContext.featured_media.localFile.childImageSharp.fluid}
           />
         </div>
+        <div className="right">
+          <form
+            name="contact-form"
+            method="POST"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+          >
+            <p class="hidden">
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Email: <input type="text" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <div data-netlify-recaptcha="true"></div>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
       </div>
     </ContactWrapper>
-    <div className="right">
-      <form
-        name="contact"
-        method="POST"
-        netlify-honeypot="bot-field"
-        data-netlify-recaptcha="true"
-        data-netlify="true"
-      >
-        <p class="hidden">
-          <label>
-            Don’t fill this out if you're human: <input name="bot-field" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Email: <input type="text" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <div data-netlify-recaptcha="true"></div>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
   </Layout>
 )
 
